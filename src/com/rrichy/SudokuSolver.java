12ef91274de1b44935dbc8b5219b10b0d075baa5
rrichy <rasulrichy@gmail.com>
2021-08-17 13:18:12 +0800

    update
    added test cases for 4x4 to 25x25
    
    todo: board validity

--- src/com/rrichy/SudokuSolver.java
+++ src/com/rrichy/SudokuSolver.java
@@ -12,40 +12,68 @@ public class SudokuSolver {
     private final ArrayList<DLXNode[]> dlx;
     private final ArrayList<DLXNode> dlxHeaders;
     private final HashSet<Integer> rowFilter = new HashSet<>();
-    private final HashSet<DLXNode> headerFilter = new HashSet<>();
-    private final ArrayList<int[]> clues = new ArrayList<>();
     private final ArrayList<int[]> solution = new ArrayList<>();
     private final ArrayList<int[][]> solutions = new ArrayList<>();
 
     public SudokuSolver(int[][] p) {
-        if(p.length != p[0].length) System.out.println("The puzzle is not a square");
         this.size = p.length;
+
+        // Check first if board is valid
+
+        if(p.length != p[0].length) System.out.println("The puzzle is not a square");
         this.matrixRow = size*size*size;
         this.matrixCol = size*size*4;
         this.dlx = new ArrayList<>();
         this.dlxHeaders = new ArrayList<>();
 
-        // transform the puzzle clues into an array of [row, col, val]
+        // transform the puzzle clues into an array of [row, col, val] and adding then to the solution
+//        for(int row = 0; row < size; row++) {
+//            for(int col = 0; col < size; col++) {
+//                if(p[row][col] != 0) {
+//                    int[] clue = new int[] {row, col, p[row][col]};
+//                    this.clues.add(clue);
+//                }
+//            }
+//        }
+
+
+        createDLXMatrix();
+
+        // For every clue in the given board, add it to the solution
         for(int row = 0; row < size; row++) {
             for(int col = 0; col < size; col++) {
                 if(p[row][col] != 0) {
-                    int[] clue = new int[] {row, col, p[row][col]};
-                    this.clues.add(clue);
+                    int digit = p[row][col];
+                    this.solution.add(new int[] {row, col, digit});
+
+                    int colCoverIndex = row * size + col;
+                    DLXNode column = rootNode.right;
+
+                    while(column.matCol != colCoverIndex) column = column.right;
+                    coverColumn(column);
+
+                    DLXNode node = column.bottom;
+                    while(node.value != digit) node = node.bottom;
+
+                    for(DLXNode nodeR = node.right; nodeR != node; nodeR = nodeR.right) coverColumn(nodeR);
                 }
             }
         }
 
-        // Check first if board is valid
-
-        createDLXMatrix();
         getSolved();
 
         System.out.println("No more solutions found.");
-        System.out.println("Number of solution/s: " + solutions.size());
+        System.out.println("Number of solution/s: " + numSolutions);
     }
 
+//    private boolean boardIsValid(int[][] board) {
+//        for(int i = 0; i < size; i++) if(board[i].length != size) return false;
+//
+//
+//    }
+
     // Sudoku will always have 4 constraints. All rows/cols/grid should have a digit
-    // from 1-9 with no repeat. All cell should be filled.
+    // from 1 to its size with no repeat. All cell should be filled.
     public void createDLXMatrix() {
         // creating all the header nodes at the same time, linking then to each other
         DLXNode current = rootNode;
@@ -83,10 +111,10 @@ public class SudokuSolver {
                     col[colIndex] = new DLXNode(curRow, curCol, val+1, dlxHeaders.get(colIndex + square*2), matRow, colIndex + square*2, -1);
                     grid[gridIndex] = new DLXNode(curRow, curCol, val+1, dlxHeaders.get(gridIndex + square*3), matRow, gridIndex + square*3, -1);
 
-                    linkLeftRight(cell[cellIndex], row[rowIndex]);
-                    linkLeftRight(row[rowIndex], col[colIndex]);
-                    linkLeftRight(col[colIndex], grid[gridIndex]);
-                    linkLeftRight(grid[gridIndex], cell[cellIndex]);
+                    linkLR(cell[cellIndex], row[rowIndex]);
+                    linkLR(row[rowIndex], col[colIndex]);
+                    linkLR(col[colIndex], grid[gridIndex]);
+                    linkLR(grid[gridIndex], cell[cellIndex]);
 
                     DLXNode[] constraints = new DLXNode[matrixCol];
                     System.arraycopy(cell, 0, constraints, 0, cell.length);
@@ -108,35 +136,22 @@ public class SudokuSolver {
             int matRowIndex = 0;
             while(instances < size) {
                 if(dlx.get(matRowIndex)[matColIndex] != null) {
-                    linkUpDown(current, dlx.get(matRowIndex)[matColIndex]);
+                    linkUD(current, dlx.get(matRowIndex)[matColIndex]);
                     current = dlx.get(matRowIndex)[matColIndex];
                     instances++;
                 }
                 matRowIndex++;
             }
-            linkUpDown(current, dlxHeaders.get(matColIndex));
-        }
-
-        for(int[] clue : clues) {
-            this.solution.add(clue);
-            int remCol = clue[0] * size + clue[1];
-            DLXNode header = rootNode.right;
-            while(header.matCol != remCol) header = header.right;
-
-            coverColumn(header);
-            DLXNode clueCell = header.bottom;
-            while(clueCell.value != clue[2]) clueCell = clueCell.bottom;
-
-            for(DLXNode nodeRight = clueCell.right; nodeRight != clueCell; nodeRight = nodeRight.right) coverColumn(nodeRight);
+            linkUD(current, dlxHeaders.get(matColIndex));
         }
     }
 
-    private void linkLeftRight(@NotNull DLXNode posA, @NotNull DLXNode posB) { // posA is on left of posB, posB is on right of posA
+    private void linkLR(@NotNull DLXNode posA, @NotNull DLXNode posB) { // posA is on left of posB, posB is on right of posA
         posA.setRight(posB);
         posB.setLeft(posA);
     }
 
-    private void linkUpDown(@NotNull DLXNode posA, @NotNull DLXNode posB) { // posA is on top of posB, posB is below posA
+    private void linkUD(@NotNull DLXNode posA, @NotNull DLXNode posB) { // posA is on top of posB, posB is below posA
         posA.setBottom(posB);
         posB.setTop(posA);
     }
@@ -181,25 +196,26 @@ public class SudokuSolver {
             saveSolution();
             numSolutions++;
             System.out.println("Solution " + numSolutions + ":");
-            printBoard(solutions.get(solutions.size() - 1));
+            printBoard(solutions.get(numSolutions - 1));
 
-            boolean validInput = false;
-
-            while(!validInput) {
-                Scanner ask = new Scanner(System.in);
-                System.out.println("Continue searching? Enter Y/N:");
-                String input = ask.nextLine();
-
-                if(input.equals("Y")) {
-                    this.continueSolving = true;
-                    validInput = true;
-                }
-                else if(input.equals("N")) {
-                    this.continueSolving = false;
-                    validInput = true;
-                }
-                else System.out.println("Error input!");
-            }
+            this.continueSolving = false;
+//            boolean validInput = false;
+//
+//            while(!validInput) {
+//                Scanner ask = new Scanner(System.in);
+//                System.out.println("Continue searching? Enter Y/N:");
+//                String input = ask.nextLine();
+//
+//                if(input.equals("Y")) {
+//                    this.continueSolving = true;
+//                    validInput = true;
+//                }
+//                else if(input.equals("N")) {
+//                    this.continueSolving = false;
+//                    validInput = true;
+//                }
+//                else System.out.println("Error input!");
+//            }
         }
         else {
             DLXNode header = findLeastColumn();
@@ -293,5 +309,9 @@ public class SudokuSolver {
         for(int[] sol : solution) singleSolution[sol[0]][sol[1]] = sol[2];
 
         solutions.add(singleSolution);
+    }
+
+    public int[][] solve() {
+        return solutions.get(0);
     }
 }
